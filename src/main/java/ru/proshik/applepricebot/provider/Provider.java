b0408c57b161c50fa3ca3085a7d603a232fc52ac
proshik <proxoraleksandrovich@gmail.com>
2018-02-10 12:28:51 +0300

    Fix a bit mistake with package naming.

--- src/main/java/ru/proshik/applepricebot/provider/Provider.java
+++
@@ -1,9 +0,0 @@
-package ru.proshik.applepricebot.provider;
-
-import ru.proshik.applepricebot.exception.ProviderParseException;
-import ru.proshik.applepricebot.storage.model.Fetch;
-
-public interface Provider {
-
-    Fetch screening() throws ProviderParseException;
-}
